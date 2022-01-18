cd5c627a5e4bfb126c74600f2479245f3073cbba
Eric Martin Feltham <eric.feltham@yale.edu>
2022-01-18 16:48:40 -0500

    add treatment history matching with exposure

--- src/setup.jl
+++ src/setup.jl
@@ -20,6 +20,22 @@ function protest_treatmentcategories(x)
 end
 
 """
+    rally_treatmentcategories(x)
+
+(For use as input to match!)
+Rally treatment history categories.
+We look at total count in the pre-treatment crossover
+period for both the treated unit and its (potential) match.
+If the totals, for each, in the same period fall into the same cateory,
+we allow the match.
+
+The exposures are accounted for directly in match!
+"""
+function rally_treatmentcategories(x)
+  return x <= 0 ? 0 : 1
+end
+
+"""
     covariateset(vn::VariableNames, outcome::Symbol; modeltype::String = "epi")
 
 Return the covariates for the specified model type ("epi", "nomob", "full").
