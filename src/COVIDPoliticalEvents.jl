366de4f589c20cd35eefb01e4dca3800f4fea411
Eric Martin Feltham <eric.feltham@yale.edu>
2022-12-30 18:08:17 -0500

    no plots

--- src/COVIDPoliticalEvents.jl
+++ src/COVIDPoliticalEvents.jl
@@ -6,7 +6,7 @@ module COVIDPoliticalEvents
   import CSV, HTTP
   import JLD2:load_object
   import TSCSMethods:mean,std
-  using CairoMakie
+  # using CairoMakie
   import ShiftedArrays:lead
   import FileIO.load
   import Indicators:sma
@@ -35,7 +35,7 @@ module COVIDPoliticalEvents
     primary_filter, filter_treated,
     merge_Rt_data,
     # plotting
-    mk_covpal,
+    # mk_covpal,
     # spillover
     countyspillover_assignment, exclude_border_counties,
     # threshold
@@ -44,10 +44,11 @@ module COVIDPoliticalEvents
     regionate, datestrat, primarydistancestrat,
     add_recent_events!,
     # supplemental plotting
-    turnout_pl, rescheduled_pl,
-    primary_mob_pl, ga_mob_pl, rally_mob_pl, protest_mob_pl,
-    exposure_shift, protest_size_hists,
-    primary_panel, blm_panel,
+    # turnout_pl, rescheduled_pl,
+    # primary_mob_pl, ga_mob_pl, rally_mob_pl, protest_mob_pl,
+    # exposure_shift, protest_size_hists,
+    # primary_panel, blm_panel,
+    #
     protest_treatmentcategories,
     rally_treatmentcategories,
     finish_data, indicatetreatments, make_weekly,
