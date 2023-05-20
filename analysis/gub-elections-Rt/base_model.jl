8c84f17c0e3e6d6f3e580c150f22ad4c409cd125
Eric Martin Feltham <eric.feltham@yale.edu>
2023-05-19 21:08:48 -0400

    updated model to current

--- analysis/gub-elections-Rt/base_model.jl
+++ analysis/gub-elections-Rt/base_model.jl
@@ -1,5 +1,30 @@
 # base_model.jl
 
+using TSCSMethods, COVIDPoliticalEvents
+push!(ARGS, "full")
+using Random
+
+scenario = "";
+F = 0:20; L = -30:-1
+refinementnum = 5; iters = 10000;
+prefix = ""
+
+# setup
+# dat_store, trump_stratassignments, trump_labels, trump_stratifier, pr_vars, trump_variables, savepath = dataload(
+#     "/Users/emf/Library/Mobile Documents/com~apple~CloudDocs/Yale/yale research/COVID19/covid-19-data/data/cvd_dat_use.jld2",
+#     "post out/"
+# )
+
+savepath = "post out/"
+
+import JLD2
+
+# JLD2.save_object("dataload.jld2", [dat_store, trump_stratassignments, trump_labels, trump_stratifier, pr_vars, trump_variables])
+
+dat, trump_stratassignments, trump_labels, trump_stratifier, pr_vars, trump_variables = JLD2.load_object("dataload.jld2")
+
+vn = VariableNames()
+
 include("preamble.jl");
 
 @time match!(model, dat);
@@ -15,11 +40,15 @@ model = trim_model(model)
   refinementnum = refinementnum, dobalance = true, doestimate = true
 );
 
-@time calmodel, refcalmodel = autobalance(
+@time calmodel, refcalmodel, overall = autobalance(
   model, dat;
   calmin = 0.08, step = 0.05,
-  initial_bals = Dict(vn.cdr => 0.25)
+  initial_bals = Dict(:Rt => 0.25),
+  dooverall = true,
+  bayesfactor = true
 );
+
+overall
 
 recordset = makerecords(
   dat, savepath, [model, refinedmodel, calmodel, refcalmodel]
